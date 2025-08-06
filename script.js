2b12acac931118bd87908852cc50505720e2a057
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-08-06 23:05:26 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Load the Lottie animation
+lottie.loadAnimation({
+  container: document.getElementById('lottie-container'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'animation.json' // Local animation file
+});
